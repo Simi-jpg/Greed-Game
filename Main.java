7ce253dc9cbd5ee05acef2e199988cc806d28f4d
Simi Adeniyi <simiadeniyi97@gmail.com>
2025-08-30 17:42:13 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
 /**
  * NAME: SIMI ADENIYI
- * STUDENT NUMBER: 7964063
- * COURSE: COMP 2150
- * INSTRUCTOR: PROF HEATHER MATTHESON
- * ASSIGNMENT: 3 (Greed Game)
- * QUESTION: STAGES 0 - 3
  * 
  * 
  * REMARKS: This is the main class for the Greed Game. It creates the menu, player, game board, game logic, and game objects, and then runs the game.
